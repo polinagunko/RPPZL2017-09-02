c6b313ae521da1697741b54182f0280cc4f10b88
Victorya <shtanovavika@gmail.com>
2017-09-11 13:38:28 +0300

    creat packadge

--- src/by/it/group573602/shtanova/lesson01/FiboB.java
+++ src/by/it/group573602/shtanova/lesson01/FiboB.java
@@ -20,17 +20,15 @@ public class FiboB {
 
         //вычисление чисел простым быстрым методом
         FiboB fibo = new FiboB();
-        int n =55;
+        int n =55555;
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
     }
 
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        int total;
-        BigInteger total = new Biginteger("1000000000000000");
-        BigInteger[] a = new BigInteger[total.intValue()];
-
-        int i, d=0;
+        BigInteger[] a = new BigInteger[n];
+        BigInteger a1=  new BigInteger("99999999999999999999");
+        int i;
         if (n<=2)
             return BigInteger.ONE;
         else {
@@ -38,12 +36,10 @@ public class FiboB {
             a[1]=BigInteger.ONE;
             for (i = 2; i < n; i++) {
                 a[i] = a[i - 1]. add( a[i - 2]);
-                System.out.printf("a[%d]=%d \n\t ", i, a[i]);
-
+                a1=a[i];
             }
-
-            return a[i];
         }
+        return a1;
     }
 }
 
