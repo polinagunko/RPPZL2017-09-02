76eed71528de579187fef1921e947e552048e4d7
ivansteff <ivansteff@gmail.com>
2017-09-15 22:58:10 +0300

    Lesson 1-1

--- src/by/it/group573601/Stephanovich/Lesson1/FiboA.java
+++ src/by/it/group573601/Stephanovich/Lesson1/FiboA.java
@@ -29,7 +29,12 @@ public class FiboA {
     private int calc(int n) {
         //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
         //время O(2^n)
-        return 0;
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        else {
+            return calc(n - 1) + calc(n - 2);
+        }
     }
 
 
