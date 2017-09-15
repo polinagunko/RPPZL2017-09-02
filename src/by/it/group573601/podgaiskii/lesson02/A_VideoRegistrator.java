a0bee4f7118e699fa559ca4ff2008aac2ab0cef7
podgaiskii <xergen13@gmail.com>
2017-09-15 23:49:22 +0300

    lesson01 & lesson02_A

---
+++ src/by/it/group573601/podgaiskii/lesson02/A_VideoRegistrator.java
@@ -0,0 +1,42 @@
+package by.it.group573601.podgaiskii.lesson02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class A_VideoRegistrator {
+
+    public static void main(String[] args) {
+        A_VideoRegistrator instance=new A_VideoRegistrator();
+        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        System.out.println(starts);                            //покажем моменты старта
+    }
+    //модификаторы доступа опущены для возможности тестирования
+    public List<Double> calcStartTimes(double[] events, double workDuration){
+        //events - события которые нужно зарегистрировать
+        //timeWorkDuration время работы видеокамеры после старта
+        List<Double> result;
+        result = new ArrayList<>();
+        int i=0;
+        Arrays.sort(events);
+
+        while(i != events.length - 1) {
+            result.add(events[i]);
+            Double start = events[i];
+            Double finish = events[i] + workDuration;
+            while (events[i] < finish)
+                i++;
+        }
+                                                //пока есть незарегистрированные события
+                                                //получим одно событие по левому краю
+                                                //и запомним время старта видеокамеры
+                                                //вычислим момент окончания работы видеокамеры
+                                                //и теперь пропустим все покрываемые события
+                                                //за время до конца работы, увеличивая индекс
+
+
+
+        return result;                        //вернем итог
+    }
+}
