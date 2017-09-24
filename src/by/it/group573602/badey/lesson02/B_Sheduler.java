a22e6bad4497d97ebc0f3030ffc4f71316b7fe98
NastyaBadey <nastya_badey@mail.ru>
2017-09-24 19:43:22 +0300

    B_Sheduler

--- src/by/it/group573602/badey/lesson02/B_Sheduler.java
+++ src/by/it/group573602/badey/lesson02/B_Sheduler.java
@@ -1,6 +1,8 @@
 package by.it.group573602.badey.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class B_Sheduler {
@@ -19,16 +21,21 @@ public class B_Sheduler {
             return "("+ start +":" + stop + ")";
         }
     }
+    class EventComparator implements Comparator<Event>{
+        public int compare(Event o1, Event o2){
+            return Integer.compare(o1.stop,o2.stop);
+        }
+    }
 
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
         Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                            new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                            new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                            new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                            new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                            new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
-                          };
+                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        };
 
         List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
         System.out.println(starts);                                 //покажем рассчитанный график занятий
@@ -39,15 +46,17 @@ public class B_Sheduler {
         //в период [from, int] (включительно).
         //оптимизация проводится по наибольшему числу непересекающихся событий.
         //начало и конец событий могут совпадать.
+        //ваше решение.
         List<Event> result;
         result = new ArrayList<>();
-        //ваше решение.
-
-
-
-
-
-
+        Arrays.sort(events,new EventComparator());
+        int i=1;
+        result.add(events[0]);
+        while(i<events.length-1){
+            if(events[i].start >= result.get(result.size()-1).stop)
+                result.add(events[i]);
+            i++;
+        }
         return result;                        //вернем итог
     }
 }
