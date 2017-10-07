1c5629c766cdc04cc407d40e0de397ed79e1d830
vabishchevich <vabishchevich94@mail.ru>
2017-10-07 20:53:00 +0300

    Lesson2 (A, B, C)

--- src/by/it/group573602/vabishchevich/lesson02/A_VideoRegistrator.java
+++ src/by/it/group573602/vabishchevich/lesson02/A_VideoRegistrator.java
@@ -14,11 +14,11 @@ public class A_VideoRegistrator {
     public static void main(String[] args) {
         A_VideoRegistrator instance=new A_VideoRegistrator();
         double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        List<Double> starts=instance.calcStartTimes(events); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts); //покажем моменты старта
     }
     //модификаторы доступа опущены для возможности тестирования
-    List<Double> calcStartTimes(double[] events, double workDuration){
+    List<Double> calcStartTimes(double[] events){
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
@@ -27,7 +27,7 @@ public class A_VideoRegistrator {
         Arrays.sort(events); //T(Sort) = O(n Log n)
         while (i < events.length-1){
             result.add(events[i]);
-            double r = events[i] + workDuration;
+            double r = events[i] + (double) 1;
             while (events[i] < r){
                 i = i + 1;
             }
