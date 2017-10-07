475aca7d0862b60ae8a234986ef8bb52f107fbea
vabishchevich <vabishchevich94@mail.ru>
2017-10-07 20:54:42 +0300

    Lesson2 (A, B, C)

--- src/by/it/group573602/vabishchevich/lesson01/FiboA.java
+++ src/by/it/group573602/vabishchevich/lesson01/FiboA.java
@@ -4,12 +4,10 @@ import java.math.BigInteger;
 
 /*
  * Вам необходимо выполнить рекурсивный способ вычисления чисел Фибоначчи
- */
+*/
 
 public class FiboA {
-
     private long startTime = System.currentTimeMillis();
-
     private long time() {
         return System.currentTimeMillis() - startTime;
     }
@@ -25,14 +23,12 @@ public class FiboA {
         System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
     }
 
-
     private int calc(int n) {
         //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
         //время O(2^n)
         if (n<=1) return n;
         else return (calc(n-1)+calc(n-2));
     }
-
 
     BigInteger slowA(Integer n) {
         //рекурсия
@@ -42,8 +38,4 @@ public class FiboA {
         if(n<=1) return BigInteger.valueOf(n);
         else return (slowA(n-1).add(slowA(n-2)));
     }
-
-
-
 }
-
