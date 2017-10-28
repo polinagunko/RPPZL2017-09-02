eff335bf739e5cf3ca05d51b0d7048082c220cf0
Horuzhenko Maria <rivulet798@gmail.com>
2017-10-28 11:15:54 +0300

    updated fork

---
+++ src/by/it/group573601/horuzhenko/lesson04/B_MergeSort.java
@@ -0,0 +1,81 @@
+package by.it.group573601.horuzhenko.lesson04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Реализуйте сортировку слиянием для одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо отсортировать полученный массив.
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2 2 3 9 9
+*/
+public class B_MergeSort {
+
+
+    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массива
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+            System.out.println(a[i]);
+        }
+        a = sortMerge(a);
+        return a;
+    }
+
+    private static int[] sortMerge(int[] arr) {
+        int len = arr.length;
+        if (len < 2) return arr;
+        int middle = len / 2;
+        return merge(sortMerge(Arrays.copyOfRange(arr, 0, middle)),
+                sortMerge(Arrays.copyOfRange(arr, middle, len)));
+    }
+
+    private static int[] merge(int[] arr_1, int[] arr_2) {
+        int len_1 = arr_1.length, len_2 = arr_2.length;
+        int a = 0, b = 0, len = len_1 + len_2; // a, b - счетчики в массивах
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (b < len_2 && a < len_1) {
+                if (arr_1[a] > arr_2[b]) result[i] = arr_2[b++];
+                else result[i] = arr_1[a++];
+            } else if (b < len_2) {
+                result[i] = arr_2[b++];
+            } else {
+                result[i] = arr_1[a++];
+            }
+        }
+        return result;
+    }
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataB.txt");
+        B_MergeSort instance = new B_MergeSort();
+        int[] result = instance.getMergeSort(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+
+}
