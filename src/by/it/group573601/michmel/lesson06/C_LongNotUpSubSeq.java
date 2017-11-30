8da452ef2c9fb2c19c9a76ffb56573e1dcfd874f
Михмель <nastiam98@mail.ru>
2017-11-30 04:00:24 +0300

    lesson06

---
+++ src/by/it/group573601/michmel/lesson06/C_LongNotUpSubSeq.java
@@ -0,0 +1,99 @@
+package by.it.group573601.michmel.lesson06;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Задача на программирование: наибольшая невозрастающая подпоследовательность
+
+Дано:
+    целое число 1<=n<=1E5 ( ОБРАТИТЕ ВНИМАНИЕ НА РАЗМЕРНОСТЬ! )
+    массив A[1…n] натуральных чисел, не превосходящих 2E9.
+
+Необходимо:
+    Выведите максимальное 1<=k<=n, для которого гарантированно найдётся
+    подпоследовательность индексов i[1]<i[2]<…<i[k] <= длины k,
+    для которой каждый элемент A[i[k]] не больше любого предыдущего
+    т.е. для всех 1<=j<k, A[i[j]]>=A[i[j+1]].
+
+    В первой строке выведите её длину k,
+    во второй - её индексы i[1]<i[2]<…<i[k]
+    соблюдая A[i[1]]>=A[i[2]]>= ... >=A[i[n]].
+
+    (индекс начинается с 1)
+
+Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+
+    Sample Input:
+    5
+    5 3 4 4 2
+
+    Sample Output:
+    4
+    1 3 4 5
+*/
+
+
+public class C_LongNotUpSubSeq {
+
+    int getNotUpSeqSize(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //общая длина последовательности
+        int n = scanner.nextInt();
+        int[] m = new int[n];
+        //читаем всю последовательность
+        for (int i = 0; i < n; i++) {
+            m[i] = scanner.nextInt();
+        }
+        //тут реализуйте логику задачи методами динамического программирования (!!!)
+        int[] position = new int[n], index = new int[n];
+        for (int i = 0; i < n; i++) {
+            position[i] = 1;
+            index[i] = -1;
+            for (int j = 0; j < i; j++)
+                if (m[j] >= m[i] && position[j] + 1 > position[i]) {
+                    position[i]++;
+                    index[i] = j;
+                }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, position[i]);
+        }
+
+        int[] res = new int[result];
+
+        int value = 0;
+        for (int i = 1; i < n; i ++)
+            if (position[i] > position[value])
+                value = i;
+
+        int i = result-1;
+        while (value >= 0){
+            res[i] = value + 1;
+            i--;
+            value = index[value];
+        }
+        System.out.println(Arrays.toString(res));
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group573601/michmel/lesson06/dataC.txt");
+        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
+        int result = instance.getNotUpSeqSize(stream);
+        System.out.print(result);
+    }
+
+}
