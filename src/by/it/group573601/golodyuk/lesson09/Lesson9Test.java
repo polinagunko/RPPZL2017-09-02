295448ab549e1b4865431acf6933372bd6e06d42
Yana <yana@yandex.ru>
2017-12-22 20:09:27 +0300

    lesson9

---
+++ src/by/it/group573601/golodyuk/lesson09/Lesson9Test.java
@@ -0,0 +1,30 @@
+package golodyuk.lesson09;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Lesson9Test {
+    @Test
+    public void A() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson09/dataA.txt");
+        A_Knapsack instance = new A_Knapsack();
+        int res=instance.getMaxWeight(stream);
+        assertEquals("A failed", res, 14);
+    }
+
+    @Test
+    public void B() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson09/dataB.txt");
+        B_Knapsack instance = new B_Knapsack();
+        int res=instance.getMaxWeight(stream);
+        assertEquals("B failed", res, 9);
+    }
+
+
+}
