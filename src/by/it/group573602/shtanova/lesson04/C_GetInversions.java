0d3978b9091a2e29f6c8d96adaed394a6fd0eb6b
Victorya <shtanovavika@gmail.com>
2018-01-08 21:17:32 +0300

    Lesson 3-9

---
+++ src/by/it/group573602/shtanova/lesson04/C_GetInversions.java
@@ -0,0 +1,105 @@
+package by.it.group573602.shtanova.lesson04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Рассчитать число инверсий одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо посчитать число пар индексов 1<=i<j<n, для которых A[i]>A[j]A[i]>A[j].
+
+    (Такая пара элементов называется инверсией массива.
+    Количество инверсий в массиве является в некотором смысле
+    его мерой неупорядоченности: например, в упорядоченном по неубыванию
+    массиве инверсий нет вообще, а в массиве, упорядоченном по убыванию,
+    инверсию образуют каждые (т.е. любые) два элемента.
+    )
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2
+
+Головоломка (т.е. не обязательно).
+Попробуйте обеспечить скорость лучше, чем O(n log n) за счет многопоточности.
+Докажите рост производительности замерами времени.
+Большой тестовый массив можно прочитать свой или сгенерировать его программно.
+*/
+
+
+public class C_GetInversions {
+
+    int count=0;
+
+    int calc(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массив
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int result = 0;
+        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
+
+        M_sort(a);
+        result= count;
+
+
+
+
+
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+    private int[] sort(int[] array1, int[] array2) {
+        int x = 0, y = 0;
+        int[] merged = new int[array1.length + array2.length];
+        for (int i = 0; i < merged.length; i++) {
+            if (y == array2.length || (x < array1.length && array1[x] <= array2[y])) {
+                merged[i] = array1[x];
+                x++;
+            } else {
+                count += array1.length - x;
+                merged[i] = array2[y];
+                y++;
+            }
+        }
+        return merged;
+    }
+    private int[] M_sort(int[] mass){
+        if(mass.length==1)
+            return mass;
+            int length=mass.length;
+            int middle=mass.length/2;
+            int[] Left= new int[middle];
+            int[] Right= new int[length-middle];
+            System.arraycopy(mass,0,Left,0,middle);
+            System.arraycopy(mass,middle,Right,0,length-middle);
+            Left=M_sort(Left);
+            Right=M_sort(Right);
+            return sort(Left,Right);
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataC.txt");
+        C_GetInversions instance = new C_GetInversions();
+        //long startTime = System.currentTimeMillis();
+        int result = instance.calc(stream);
+        //long finishTime = System.currentTimeMillis();
+        System.out.print(result);
+    }
+}
---
+++ src/by/it/group573602/shtanova/lesson09/A_Knapsack.java
@@ -0,0 +1,83 @@
+package by.it.group573602.shtanova.lesson09;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: рюкзак с повторами
+
+Первая строка входа содержит целые числа
+    1<=W<=100000     вместимость рюкзака
+    1<=n<=300        сколько есть вариантов золотых слитков
+                     (каждый можно использовать множество раз).
+Следующая строка содержит n целых чисел, задающих веса слитков:
+  0<=w[1]<=100000 ,..., 0<=w[n]<=100000
+
+Найдите методами динамического программирования
+максимальный вес золота, который можно унести в рюкзаке.
+
+
+Sample Input:
+10 3
+1 4 8
+Sample Output:
+10
+
+Sample Input 2:
+15 3
+2 8 16
+Sample Output 2:
+14
+
+*/
+
+public class A_Knapsack {
+
+    int getMaxWeight(InputStream stream) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        Scanner scanner = new Scanner(stream);
+        int w = scanner.nextInt();
+        int n = scanner.nextInt();
+        int gold[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            gold[i] = scanner.nextInt();
+            System.out.println("Вес: " + gold[i]);
+        }
+        System.out.println("Вместимость " + w);
+        System.out.println("Варианты " + n);
+        int result = 0;
+        int[] array = new int[w];
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < n; j++) {
+                if (gold[j] <= i) {
+                    int wi = gold[j];
+                    int ci = gold[j];
+                    int wigth = array[i - wi] + ci;
+                    if (wigth > array[i]) {
+                        array[i] = wigth;
+                        result = array[i];
+                    } else {
+                        wigth = array[i];
+                        result = wigth;
+                    }
+                }
+            }
+        }
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson09/dataA.txt");
+        A_Knapsack instance = new A_Knapsack();
+        int res = instance.getMaxWeight(stream);
+        System.out.println(res);
+    }
+}
+
